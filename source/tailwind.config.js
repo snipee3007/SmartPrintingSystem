b5e1767969d7a288d1fefd5c14c37a2cc20651f4
Snipee3007 <zennyteam@gmail.com>
2023-12-13 15:28:44 +0700

    Update Personal Information view

--- source/tailwind.config.js
+++ source/tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}"
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      Inter: ["Inter", "sans-serif"],
+      MyFont: ['"My Font"', "serif"], // Ensure fonts with spaces have " " surrounding it.
+    },
   },
   plugins: [],
-}
-
+};
