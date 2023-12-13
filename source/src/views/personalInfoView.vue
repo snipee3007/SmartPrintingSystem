25eca8c4da3d7ca85aa4b8fc66a01c9356545793
Snipee3007 <zennyteam@gmail.com>
2023-12-13 15:34:46 +0700

    fix Personal Information View Style

--- source/src/views/personalInfoView.vue
+++ source/src/views/personalInfoView.vue
@@ -8,7 +8,7 @@
         <div class="flex flex-row flex-wrap mb-[2%] items-center">
           <p class="body-content h-full w-[30%]">Họ</p>
           <div
-            class="w-[30%] py-[1%] border-black border rounded-xl bg-gray-100 indent-2"
+            class="body-content-1 w-[30%] py-[1%] border-black border rounded-xl bg-gray-100 indent-2"
           >
             Lê
           </div>
@@ -16,7 +16,7 @@
         <div class="flex flex-row flex-wrap mb-[2%] items-center">
           <p class="body-content h-full w-[30%]">Tên</p>
           <div
-            class="w-[30%] py-[1%] border-black border rounded-xl bg-gray-100 indent-2"
+            class="body-content-1 w-[30%] py-[1%] border-black border rounded-xl bg-gray-100 indent-2"
           >
             Cồn
           </div>
@@ -24,7 +24,7 @@
         <div class="flex flex-row flex-wrap mb-[2%] items-center">
           <p class="body-content h-full w-[30%]">Mã số sinh viên</p>
           <div
-            class="w-[30%] py-[1%] border-black border rounded-xl bg-gray-100 indent-2"
+            class="body-content-1 w-[30%] py-[1%] border-black border rounded-xl bg-gray-100 indent-2"
           >
             2152445
           </div>
@@ -37,12 +37,12 @@
         <div class="flex flex-row flex-wrap mb-[2%] items-center">
           <p class="body-content h-full w-[30%]">Số trang in còn lại</p>
           <div
-            class="w-[7%] py-[1%] border-black border rounded-xl bg-gray-100 text-center"
+            class="body-content-1 w-[7%] py-[1%] border-black border rounded-xl bg-gray-100 text-center"
           >
             50
           </div>
           <button
-            class="w-[27%] py-[0.5%] bg-sky-600 text-white ml-[5%] border-black border rounded-xl px-[2%]"
+            class="body-content-1 w-[27%] py-[0.5%] bg-sky-600 text-white ml-[5%] border-black border rounded-xl px-[2%]"
           >
             Mua thêm giấy
           </button>
@@ -50,12 +50,12 @@
         <div class="flex flex-row flex-wrap mb-[2%] items-center">
           <p class="body-content h-full w-[30%]">Số trang đã in</p>
           <div
-            class="w-[7%] py-[1%] border-black border rounded-xl bg-gray-100 text-center"
+            class="body-content-1 w-[7%] py-[1%] border-black border rounded-xl bg-gray-100 text-center"
           >
             69
           </div>
           <button
-            class="w-[27%] py-[0.5%] bg-sky-600 text-white ml-[5%] border-black border rounded-xl px-[2%]"
+            class="body-content-1 w-[27%] py-[0.5%] bg-sky-600 text-white ml-[5%] border-black border rounded-xl px-[2%]"
           >
             Xem lịch sử in
           </button>
@@ -63,13 +63,13 @@
         <div class="flex flex-row flex-wrap mb-[2%] items-center">
           <p class="body-content h-full w-[30%]">Số trang A4</p>
           <div
-            class="w-[7%] py-[1%] border-black border rounded-xl bg-gray-100 text-center"
+            class="body-content-1 w-[7%] py-[1%] border-black border rounded-xl bg-gray-100 text-center"
           >
             35
           </div>
           <p class="body-content h-full w-[20%] ml-[5%]">Số trang A3</p>
           <div
-            class="w-[7%] py-[1%] border-black border rounded-xl bg-gray-100 text-center"
+            class="body-content-1 w-[7%] py-[1%] border-black border rounded-xl bg-gray-100 text-center"
           >
             15
           </div>
@@ -92,6 +92,14 @@
   color: #000;
   font-family: Inter;
   font-size: 3vh;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+}
+.body-content-1 {
+  color: #000;
+  font-family: Inter;
+  font-size: 2.5vh;
   font-style: normal;
   font-weight: 400;
   line-height: normal;
