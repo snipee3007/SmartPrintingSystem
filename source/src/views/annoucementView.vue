20bcbe5b65add9269fd54a9dfb5008c47c32d746
Masamune <125840508+Masamuneee@users.noreply.github.com>
2023-12-14 01:28:00 +0700

    Update view

--- source/src/views/annoucementView.vue
+++ source/src/views/annoucementView.vue
@@ -1,3 +1,6 @@
 <template>
-    <div>Tính năng sắp mở</div>
+    <div class="w-screen mr-auto ml-auto bg-gray-100 max-w-6xl py-10 px-20">
+        <div class="font">Thông báo</div>
+        <div class="flex rounded-[10px] border border-black border-solid my-10 py-10 px-30"></div>
+    </div>
 </template>
