566c8ecd45a37453ff376a18adfb72c4104c4050
Masamune <125840508+Masamuneee@users.noreply.github.com>
2023-12-14 15:35:09 +0700

    Update Announcement

--- source/src/views/annoucementView.vue
+++ source/src/views/annoucementView.vue
@@ -1,6 +1,31 @@
 <template>
     <div class="w-screen mr-auto ml-auto bg-gray-100 max-w-6xl py-10 px-20">
-        <div class="font">Thông báo</div>
-        <div class="flex rounded-[10px] border border-black border-solid my-10 py-10 px-30"></div>
+        <div class="w-[1030px] h-[537px] relative">
+            <div class="w-[1030px] h-[496px] left-0 top-[41px] absolute rounded-[15px] border border-black">
+            </div>
+            <div class="w-[966px] h-[70px] left-[16px] top-[72px] absolute">
+                <div class="w-6 h-6 left-0 top-[23px] absolute flex-col justify-start items-start inline-flex">
+                    <div class="w-[18px] h-[18px] bg-slate-500 bg-opacity-25 rounded-full flex items-center justify-center">
+                        <a href="#"> 
+                            <div class="w-[11px] h-8 text-black text-xl font-normal font-['Inter']">
+                                x
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div
+                    class="w-[935px] h-[70px] left-[31px] top-0 rounded-lg border border-black bg-white absolute flex items-center justify-center">
+                    <div class="w-[865px] h-8 text-black text-xl font-normal font-['Inter']">
+                        Việc in ấn tài liệu của bạn đã hoàn tất. Vui lòng xuống phòng 202 A4 để nhận tài liệu.
+                    </div>
+                </div>
+            </div>
+            <div class="w-28 h-[26px] left-0 top-0 absolute text-black text-xl font-normal font-['Inter']">Thông báo</div>
+            <a href=#>
+                <div
+                    class="w-28 h-[26px] left-[908px] top-0 absolute text-right text-black text-xl font-normal font-['Inter']">
+                    Xoá tất cả</div>
+            </a>
+        </div>
     </div>
 </template>
